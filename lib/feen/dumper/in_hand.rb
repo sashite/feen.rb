f17c47301a68c52e7191bceb9e7eff9388ffbeab
Cyril Kato <contact@cyril.email>
2020-10-03 19:21:04 +0200

    fix(Parser::InHand): empty list behavior

--- lib/feen/dumper/in_hand.rb
+++ lib/feen/dumper/in_hand.rb
@@ -2,15 +2,19 @@
 
 module FEEN
   module Dumper
-    # The pieces in hand class.
-    #
-    # @example Serialize a list of pieces in hand grouped by sides
-    #   PiecesInHand.dump(%w[S r r b g g g g s n n n n p p p p p p p p p p p p p p p p p])
-    #   # => "S,b,g,g,g,g,n,n,n,n,p,p,p,p,p,p,p,p,p,p,p,p,p,p,p,p,p,r,r,s"
+    # The pieces in hand module.
     module InHand
       # Serialize pieces in hand lists into a string.
       #
       # @param piece_names [Array] A list of pieces in hand.
+      #
+      # @example Dump a list of pieces in hand
+      #   dump(["S", "b", "g", "g", "g", "g", "n", "n", "n", "n", "p", "p", "p", "p", "p", "p", "p", "p", "p", "p", "p", "p", "p", "p", "p", "p", "p", "r", "r", "s"])
+      #   # => "S,b,g,g,g,g,n,n,n,n,p,p,p,p,p,p,p,p,p,p,p,p,p,p,p,p,p,r,r,s"
+      #
+      # @example Dump an empty list of pieces in hand
+      #   dump([])
+      #   # => "-"
       #
       # @return [String] A string representing the pieces in hand.
       def self.dump(piece_names)
--- lib/feen/parser/in_hand.rb
+++ lib/feen/parser/in_hand.rb
@@ -12,8 +12,14 @@ module FEEN
       #   parse("S,b,g,g,g,g,n,n,n,n,p,p,p,p,p,p,p,p,p,p,p,p,p,p,p,p,p,r,r,s")
       #   # => ["S", "b", "g", "g", "g", "g", "n", "n", "n", "n", "p", "p", "p", "p", "p", "p", "p", "p", "p", "p", "p", "p", "p", "p", "p", "p", "p", "r", "r", "s"]
       #
+      # @example Parse an empty list of serialized pieces in hand
+      #   parse("-")
+      #   # => []
+      #
       # @return [Array] The list of pieces in hand grouped by players.
       def self.parse(piece_names_str)
+        return [] if piece_names_str == "-"
+
         piece_names_str.split(",")
       end
     end
